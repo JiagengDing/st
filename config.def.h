2a9d9dc9b827394df245ea2283ed184e7f26644b
jiageng <jiageng.ding@gmail.com>
2023-07-27 19:10:49 +0100

    change color scheme to tokyonight

--- config.def.h
+++ config.def.h
@@ -97,34 +97,32 @@ unsigned int tabspaces = 8;
 float alpha = 0.85;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-  /* 8 normal colors */
-  [0] = "#000000", /* black   */
-  [1] = "#ff5555", /* red     */
-  [2] = "#50fa7b", /* green   */
-  [3] = "#f1fa8c", /* yellow  */
-  [4] = "#bd93f9", /* blue    */
-  [5] = "#ff79c6", /* magenta */
-  [6] = "#8be9fd", /* cyan    */
-  [7] = "#bbbbbb", /* white   */
-
-  /* 8 bright colors */
-  [8]  = "#44475a", /* black   */
-  [9]  = "#ff5555", /* red     */
-  [10] = "#50fa7b", /* green   */
-  [11] = "#f1fa8c", /* yellow  */
-  [12] = "#bd93f9", /* blue    */
-  [13] = "#ff79c6", /* magenta */
-  [14] = "#8be9fd", /* cyan    */
-  [15] = "#ffffff", /* white   */
-
-  /* special colors */
-  [256] = "#282a36", /* background */
-  [257] = "#f8f8f2", /* foreground */
-};
-
-
-
+// static const char *colorname[] = {
+//   /* 8 normal colors */
+//   [0] = "#000000", /* black   */
+//   [1] = "#ff5555", /* red     */
+//   [2] = "#50fa7b", /* green   */
+//   [3] = "#f1fa8c", /* yellow  */
+//   [4] = "#bd93f9", /* blue    */
+//   [5] = "#ff79c6", /* magenta */
+//   [6] = "#8be9fd", /* cyan    */
+//   [7] = "#bbbbbb", /* white   */
+//
+//   /* 8 bright colors */
+//   [8]  = "#44475a", /* black   */
+//   [9]  = "#ff5555", /* red     */
+//   [10] = "#50fa7b", /* green   */
+//   [11] = "#f1fa8c", /* yellow  */
+//   [12] = "#bd93f9", /* blue    */
+//   [13] = "#ff79c6", /* magenta */
+//   [14] = "#8be9fd", /* cyan    */
+//   [15] = "#ffffff", /* white   */
+//
+//   /* special colors */
+//   [256] = "#282a36", /* background */
+//   [257] = "#f8f8f2", /* foreground */
+// };
+//
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
@@ -133,6 +131,49 @@ unsigned int defaultfg = 257;
 unsigned int defaultbg = 256;
 unsigned int defaultcs = 257;
 static unsigned int defaultrcs = 257;
+
+/* Terminal colors (16 first used in escape sequence) */
+static const char *colorname[] = {
+    /* 8 normal colors */
+    "#1d202f",
+    "#f7768e",
+    "#9ece6a",
+    "#e0af68",
+    "#7aa2f7",
+    "#bb9af7",
+    "#7dcfff",
+    "#a9b1d6",
+
+    /* 8 bright colors */
+
+    "#414868",
+    "#f7768e",
+    "#9ece6a",
+    "#e0af68",
+    "#7aa2f7",
+    "#bb9af7",
+    "#7dcfff",
+    "#c0caf5",
+
+    [255] = 0,
+
+    /* more colors can be added after 255 to use with DefaultXX */
+    "#c0caf5",
+    "#364a82",
+    "#c0caf5", /* default foreground colour */
+    "#24283b", /* default background colour */
+};
+
+/*
+ * Default colors (colorname index)
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
+
+
 
 unsigned int defaultitalic = 7;
 unsigned int defaultunderline = 7;
